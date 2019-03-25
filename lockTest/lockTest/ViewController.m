f579a8e707ccf016be3e1c75978e499209e40648
fanyl <yunlong.fan@okcoin.com>
2019-03-25 15:10:54 +0800

    【提交类型】new 【修改内容】lock test 【影响范围】demo 【风险评估】low

---
+++ lockTest/lockTest/ViewController.m
@@ -0,0 +1,251 @@
+//
+//  ViewController.m
+//  lockTest
+//
+//  Created by fanyunlong on 2017/6/15.
+//  Copyright © 2017年 fanyunlong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import <pthread.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self conditionSignalTest];
+    
+    
+    
+}
+
+// 互斥锁会使得线程阻塞，阻塞的过程又分两个阶段，第一阶段是会先空转，可以理解成跑一个 while 循环，不断地去申请加锁，在空转一定时间之后，线程会进入 waiting 状态，此时线程就不占用CPU资源了，等锁可用的时候，这个线程会立即被唤醒。
+-(void)lockTest
+{
+    //主线程中，互斥锁
+    NSLock *lock = [[NSLock alloc] init];
+    
+    //线程1
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [lock lock];
+        NSLog(@"线程1");
+        sleep(20);
+        [lock unlock];
+        NSLog(@"线程1解锁成功");
+    });
+    
+    //线程2
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        sleep(1);//以保证让线程2的代码后执行
+        [lock lock];
+        NSLog(@"线程2");
+        [lock unlock];
+    });
+}
+
+// 条件锁, NSConditionLock 还可以实现任务之间的依赖。
+-(void)conditionLockTest
+{
+    //主线程中
+    NSConditionLock *lock = [[NSConditionLock alloc] initWithCondition:0];
+    
+    //线程1
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [lock lockWhenCondition:1];//locakWhenCondition 与 lock 方法类似，加锁失败会阻塞线程，所以线程 1 会被阻塞着
+        NSLog(@"线程1");
+        sleep(2);
+        [lock unlock];
+    });
+    
+    //线程2
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        sleep(1);//以保证让线程2的代码后执行
+        if ([lock tryLockWhenCondition:0]) { // 方法就算条件不满足，也会返回 NO，不会阻塞当前线程。
+            NSLog(@"线程2");
+            [lock unlockWithCondition:2];// unlockWithCondition: 并不是当 Condition 符合条件时才解锁，而是解锁之后，修改 Condition 的值，
+            NSLog(@"线程2解锁成功");
+        } else {
+            NSLog(@"线程2尝试加锁失败");
+        }
+    });
+    
+    //线程3
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        sleep(2);//以保证让线程2的代码后执行
+        if ([lock tryLockWhenCondition:2]) {
+            NSLog(@"线程3");
+            [lock unlock];
+            NSLog(@"线程3解锁成功");
+        } else {
+            NSLog(@"线程3尝试加锁失败");
+        }
+    });
+    
+    //线程4
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        sleep(3);//以保证让线程2的代码后执行
+        if ([lock tryLockWhenCondition:2]) {
+            NSLog(@"线程4");
+            [lock unlockWithCondition:1];
+            NSLog(@"线程4解锁成功");
+        } else {
+            NSLog(@"线程4尝试加锁失败");
+        }
+    });
+}
+
+//  NSRecursiveLock 他和 NSLock 的区别在于，NSRecursiveLock 可以在一个线程中重复加锁（反正单线程内任务是按顺序执行的，不会出现资源竞争问题），NSRecursiveLock 会记录上锁和解锁的次数，当二者平衡的时候，才会释放锁，其它线程才可以上锁成功。
+-(void)recursiveLockTest
+{
+    NSRecursiveLock *lock = [[NSRecursiveLock alloc] init];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        static void (^RecursiveBlock)(int);
+        RecursiveBlock = ^(int value) {
+            [lock lock];
+            if (value > 0) {
+                NSLog(@"value:%d", value);
+                RecursiveBlock(value - 1);
+            }
+            [lock unlock];
+        };
+        RecursiveBlock(2);
+    });
+    
+    // 如果用 NSLock 的话，lock 先锁上了，但未执行解锁的时候，就会进入递归的下一层，而再次请求上锁，阻塞了该线程，线程被阻塞了，自然后面的解锁代码不会执行，而形成了死锁。而 NSRecursiveLock 递归锁就是为了解决这个问题。
+}
+
+// NSCondition 的对象实际上作为一个锁和一个线程检查器，锁上之后其它线程也能上锁，而之后可以根据条件决定是否继续运行线程，即线程是否要进入 waiting 状态，经测试，NSCondition 并不会像上文的那些锁一样，先轮询，而是直接进入 waiting 状态，当其它线程中的该锁执行 signal 或者 broadcast 方法时，线程被唤醒，继续运行之后的方法。
+-(void)conditionSignalTest
+{
+    NSCondition *lock = [[NSCondition alloc] init];
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+    //线程1
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [lock lock];
+        while (!array.count) {
+            [lock wait];
+        }
+        [array removeAllObjects];
+        NSLog(@"array removeAllObjects");
+        [lock unlock];
+    });
+    
+    //线程2
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        sleep(1);//以保证让线程2的代码后执行
+        [lock lock];
+        [array addObject:@1];
+        NSLog(@"array addObject:@1");
+        [lock signal];
+        [lock unlock];
+    });
+}
+
+// @synchronized 指令实现锁的优点就是我们不需要在代码中显式的创建锁对象，便可以实现锁的机制，但作为一种预防措施，@synchronized 块会隐式的添加一个异常处理例程来保护代码，该处理例程会在异常抛出的时候自动的释放互斥锁。@synchronized 还有一个好处就是不用担心忘记解锁了。
+
+-(void)synchronizedTest
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        @synchronized(self) {
+            sleep(2);
+            NSLog(@"线程1");
+        }
+        NSLog(@"线程1解锁成功");
+    });
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        sleep(1);
+        @synchronized(self) {
+            NSLog(@"线程2");
+        }
+    });
+}
+
+// dispatch_semaphore 和 NSCondition 类似，都是一种基于信号的同步方式，但 NSCondition 信号只能发送，不能保存（如果没有线程在等待，则发送的信号会失效）。而 dispatch_semaphore 能保存发送的信号。dispatch_semaphore 的核心是 dispatch_semaphore_t 类型的信号量。
+-(void)semaphoreTest
+{
+    dispatch_semaphore_t signal = dispatch_semaphore_create(1); // dispatch_semaphore_create(1) 方法可以创建一个 dispatch_semaphore_t 类型的信号量，设定信号量的初始值为 1。注意，这里的传入的参数必须大于或等于 0，否则 dispatch_semaphore_create 会返回 NULL。
+    
+    dispatch_time_t overTime = dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC);
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        dispatch_semaphore_wait(signal, overTime);
+        
+        sleep(2);
+        NSLog(@"线程1");
+        
+        dispatch_semaphore_signal(signal);
+    });
+    
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        sleep(1);
+        dispatch_semaphore_wait(signal, overTime);
+        NSLog(@"线程2");
+        dispatch_semaphore_signal(signal);
+    });
+}
+
+static pthread_mutex_t theLock;
+
+void *threadMethord1() {
+    pthread_mutex_lock(&theLock);
+    printf("线程1\n");
+    sleep(2);
+    pthread_mutex_unlock(&theLock);
+    printf("线程1解锁成功\n");
+    return 0;
+}
+
+void *threadMethord2() {
+    sleep(1);
+    pthread_mutex_lock(&theLock);
+    printf("线程2\n");
+    pthread_mutex_unlock(&theLock);
+    return 0;
+}
+
+-(void)pthread_mutex_test
+{
+    pthread_mutex_init(&theLock, NULL); // 首先是第一个方法，这是初始化一个锁，__restrict 为互斥锁的类型，传 NULL 为默认类型，一共有 4 类型。
+/*
+ 
+ // PTHREAD_MUTEX_NORMAL 缺省类型，也就是普通锁。当一个线程加锁以后，其余请求锁的线程将形成一个等待队列，并在解锁后先进先出原则获得锁。
+ 
+ PTHREAD_MUTEX_ERRORCHECK 检错锁，如果同一个线程请求同一个锁，则返回 EDEADLK，否则与普通锁类型动作相同。这样就保证当不允许多次加锁时不会出现嵌套情况下的死锁。
+ 
+ PTHREAD_MUTEX_RECURSIVE 递归锁，允许同一个线程对同一个锁成功获得多次，并通过多次 unlock 解锁。
+ 
+ PTHREAD_MUTEX_DEFAULT 适应锁，动作最简单的锁类型，仅等待解锁后重新竞争，没有等待队列。
+ 
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+    pthread_mutex_init(&theLock, &attr);
+    pthread_mutexattr_destroy(&attr);
+ */
+    
+    pthread_t thread;
+    pthread_create(&thread, NULL, threadMethord1, NULL);
+    
+    pthread_t thread2;
+    pthread_create(&thread2, NULL, threadMethord2, NULL);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
