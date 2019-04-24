6e05c3412bdcbcd4e4b9c89ce099a8d9672517b3
fanyl <yunlong.fan@okcoin.com>
2019-04-24 17:27:16 +0800

    【提交类型】new 【修改内容】lay demo 【影响范围】demo 【风险评估】low

---
+++ YLLayTest/LayerDemo/ViewController.m
@@ -0,0 +1,85 @@
+//
+//  ViewController.m
+//  LayerDemo
+//
+//  Created by CHT-Technology on 2017/3/22.
+//  Copyright © 2017年 CHT-Technology. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "XZMGradientProgressView.h"
+#import "XZMPieView.h"
+
+@interface ViewController ()
+{
+    XZMGradientProgressView *progressView1;
+    XZMGradientProgressView *progressView2;
+    XZMPieView *pieView;
+}
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    progressView1 = [XZMGradientProgressView
+                     gradientProgressViewWithFrame:CGRectMake(20, 100, 200, 50)
+                     style:XZMGradientProgressStyleLine
+                     showTitle:YES
+                     animation:YES];
+    
+    progressView1.gradientCGColors = @[(id)[UIColor grayColor].CGColor,
+                                       (id)[UIColor blueColor].CGColor,
+                                       (id)[UIColor orangeColor].CGColor,
+                                       (id)[UIColor purpleColor].CGColor];
+    progressView1.progress = 0.8;
+    [self.view addSubview:progressView1];
+    
+    
+    progressView2 = [XZMGradientProgressView
+                     gradientProgressViewWithFrame:CGRectMake(20, 150, 120, 200)
+                     style:XZMGradientProgressStyleCircle
+                     showTitle:YES
+                     animation:YES];
+    
+    progressView2.progress = 0.6;
+    
+    [self.view addSubview:progressView2];
+    
+    
+    pieView = [[XZMPieView alloc]initWithFrame:CGRectMake(80, 350, 200, 200)];
+    
+    [self.view addSubview:pieView];
+    
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    pieView.sectorSpace = (arc4random() % 5)/10.0;
+    [pieView setDatas:[self getDatas] colors:@[[UIColor redColor],[UIColor purpleColor]]];
+    [pieView stroke];
+    
+    [progressView1 startRendering];
+    [progressView2 startRendering];
+}
+
+- (NSArray *)getDatas {
+    
+    int cout = arc4random() % 5;
+    NSMutableArray *arr = [NSMutableArray array];
+    for (int i = 0; i < cout+1 ; i++) {
+        [arr addObject:@(arc4random()%100)];
+    }
+    
+    return arr;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
+
+
