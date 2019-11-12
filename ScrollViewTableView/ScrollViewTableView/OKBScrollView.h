80923f4ebdd4f0181a3e590056ea0321c157e6e8
fanyl <yunlong.fan@okcoin.com>
2019-11-12 18:51:58 +0800

    【提交类型】new 【修改内容】scrollview + tableview, tableview 不触发拖动问题 【影响范围】demo 【风险评估】low

---
+++ ScrollViewTableView/ScrollViewTableView/OKBScrollView.h
@@ -0,0 +1,17 @@
+//
+//  OKBScrollView.h
+//  ScrollViewTableView
+//
+//  Created by fanyl on 2019/11/12.
+//  Copyright © 2019 fanyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OKBScrollView : UIScrollView
+
+@end
+
+NS_ASSUME_NONNULL_END
