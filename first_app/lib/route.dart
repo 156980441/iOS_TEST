b2a437d145b2d6a814ec9e9e2482768a74d83557
fanyl <yunlong.fan@okcoin.com>
2020-07-26 22:45:16 +0800

    1|【提交类型】new 【修改内容】flutter 【影响范围】bb 【风险评估】low

---
+++ first_app/lib/route.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go!'),
+          onPressed: () {
+            Navigator.of(context).push(_createRoute());
+          },
+        ),
+      ),
+    );
+  }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => Page2(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return child;
+    },
+  );
+}
+
+class Page2 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Page 2'),
+      ),
+    );
+  }
+}
