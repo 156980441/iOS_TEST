d86ded7d66427692e2196760f34d343e1fb1aac8
fanyl <yunlong.fan@okcoin.com>
2020-07-27 01:32:40 +0800

    1|【提交类型】new 【修改内容】leetcode,132,死循环了 【影响范围】bb 【风险评估】low

--- 132/132/ViewController.m
+++ 132/132/ViewController.m
@@ -14,12 +14,39 @@
 
 @implementation ViewController
 
+void chars2IntArr (const char* str, int *output, unsigned int length) {
+    for (int i = 0; i < length; i++) {
+        output[i] = atoi(&str[i * 2]);
+    }
+    
+    printf("\n======\n");
+    
+    for (int i = 0; i < length; i++) {
+        printf("%d,", output[i]);
+    }
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    //获取视频播放路径
+    NSString *str = [[NSBundle mainBundle] resourcePath];
+    NSString *filePath = [NSString stringWithFormat:@"%@%@", str, @"/132_test.txt"];
+    NSData *data = [NSData dataWithContentsOfFile:filePath];
+    NSString *dataStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    const char *cStr = dataStr.UTF8String;
+    size_t num = strlen(cStr);
+    NSLog(@"%s", cStr);
+    
+    size_t num2 = ceil(strlen(cStr) / 2);
+    int *arr = malloc(sizeof(int) * num2);
+    chars2IntArr(cStr, arr, (unsigned int)num2);
+    
     // Do any additional setup after loading the view.
-//    int a[] = {0,1,2,0,1,2,0};
-    int a[] = {-2,1,2,-2,1,2};
-    bool i = find132pattern(a, sizeof(a)/sizeof(int));
+    //    int a[] = {0,1,2,0,1,2,0};
+//    int arr[] = {-2,1,2,-2,1,2};
+//    bool i = find132pattern(arr, sizeof(arr)/sizeof(int));
+    bool i = find132pattern(arr, num2);
     printf("%d", i);
 }
 
@@ -30,13 +57,15 @@ bool find132pattern(int* nums, int numsSize) {
     }
     
     int *pcuror; // 移动游标
+    int *pfirst; // 第一个元素指针
     int *plast; // 最后一个元素指针
     int tmp; // 中间变量
     
+    pfirst = nums;
     plast = &nums[numsSize - 1]; // 最后一个元素
-    while (nums < plast) {
+    while (pfirst < plast) {
         pcuror = plast;
-        while (nums < pcuror) { // 第一个是第一个，最后一个放在第二个，之后寻找第一个和最后一个中间比最后一个大的
+        while (pfirst < pcuror) { // 第一个是第一个，最后一个放在第二个，之后寻找第一个和最后一个中间比最后一个大的
             --pcuror;
             tmp = *pcuror;
             if (*plast < tmp && *nums < *plast) {
@@ -49,9 +78,9 @@ bool find132pattern(int* nums, int numsSize) {
         plast--; // 把倒数第二个当做最后个，第一个是第一个，再次遍历一次
     }
     
-    pcuror = nums;
-    nums++;
-    return find132pattern(nums, numsSize - (int)(nums - pcuror));
+    pcuror = pfirst;
+    pfirst++;
+    return find132pattern(pfirst, numsSize - (int)(pfirst - pcuror));
 }
 
 
