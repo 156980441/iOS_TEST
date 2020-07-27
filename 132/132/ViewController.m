7e5ec3b41ab738e12cbb17b9665f6d98cc5f5aaa
fanyl <yunlong.fan@okcoin.com>
2020-07-27 10:32:24 +0800

    1|【提交类型】new 【修改内容】leetcode,132, 放弃递归 【影响范围】bb 【风险评估】low

--- 132/132/ViewController.m
+++ 132/132/ViewController.m
@@ -50,7 +50,7 @@ void chars2IntArr (const char* str, int *output, unsigned int length) {
     printf("%d", i);
 }
 
-bool find132pattern(int* nums, int numsSize) {
+bool find132patternRecursion(int* nums, int numsSize) {
     
     if (numsSize == 0) {
         return false;
@@ -80,7 +80,60 @@ bool find132pattern(int* nums, int numsSize) {
     
     pcuror = pfirst;
     pfirst++;
-    return find132pattern(pfirst, numsSize - (int)(pfirst - pcuror));
+    return find132patternRecursion(pfirst, numsSize - (int)(pfirst - pcuror));
+}
+
+
+bool find132pattern(int* nums, int numsSize) {
+    
+    if (numsSize == 0) {
+        return false;
+    }
+    
+    int *pcuror; // 移动游标
+    int *pfirst; // 第一个元素指针
+    int *plast; // 最后一个元素指针
+    int tmp; // 中间变量
+    
+    pfirst = nums;
+    plast = &nums[numsSize - 1]; // 最后一个元素
+    
+    
+    while (pfirst < plast) {
+        while (pfirst < plast) {
+            pcuror = plast;
+            while (pfirst < pcuror) { // 第一个是第一个，最后一个放在第二个，之后寻找第一个和最后一个中间比最后一个大的
+                --pcuror;
+                tmp = *pcuror;
+                if (*plast < tmp && *nums < *plast) {
+                    return true;
+                }
+                else {
+                    continue;
+                }
+            }
+            plast--; // 把倒数第二个当做最后个，第一个是第一个，再次遍历一次
+        }
+        
+        pcuror = pfirst;
+        pfirst++;
+        
+        while (pfirst < plast) {
+            pcuror = plast;
+            while (pfirst < pcuror) { // 第一个是第一个，最后一个放在第二个，之后寻找第一个和最后一个中间比最后一个大的
+                --pcuror;
+                tmp = *pcuror;
+                if (*plast < tmp && *nums < *plast) {
+                    return true;
+                }
+                else {
+                    continue;
+                }
+            }
+            plast--; // 把倒数第二个当做最后个，第一个是第一个，再次遍历一次
+        }
+    }
+    return false;
 }
 
 
