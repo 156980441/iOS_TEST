65a64d3267d43a054ac467ac2993caa864b70cac
fanyl <156980441@qq.com>
2021-08-10 11:37:19 +0800

    shadow and corner

--- shadow_corner/shadow_corner/ViewController.m
+++ shadow_corner/shadow_corner/ViewController.m
@@ -18,8 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    CGFloat space = 10;
+    CGFloat space = 40;
     
+    // 圆角
     UIView *view = [[UIView alloc] initWithFrame:CGRectZero];
     view.frame = CGRectMake(100, 100, 100, 100);
     view.backgroundColor = [UIColor grayColor];
@@ -28,7 +29,7 @@
     // 这里不需要调佣以下方法
     // view.clipsToBounds = YES;
     
-    
+    // 阴影
     UIView *shadowView = [[UIView alloc] initWithFrame:CGRectZero];
     shadowView.frame = CGRectMake(100, CGRectGetMaxY(view.frame) + space, 100, 100);
     shadowView.backgroundColor = [UIColor redColor];
@@ -37,6 +38,29 @@
     shadowView.layer.shadowOpacity = 1; // default 0，0 就看不到阴影
     shadowView.layer.shadowRadius = 16; // 阴影的圆角
     [self.view addSubview:shadowView];
+    
+    // 指定圆角
+    UIView *cornerView = [[UIView alloc] initWithFrame:CGRectZero];
+    cornerView.frame = CGRectMake(100, CGRectGetMaxY(shadowView.frame) + space, 100, 100);
+    cornerView.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:cornerView];
+    
+    
+    UIBezierPath *maskPath1 = [UIBezierPath bezierPathWithRoundedRect:view.bounds
+                                                    byRoundingCorners:UIRectCornerTopLeft | UIRectCornerTopRight
+                                                          cornerRadii:CGSizeMake(24, 24)];
+    CAShapeLayer *maskLayer1 = [[CAShapeLayer alloc] init];
+    maskLayer1.frame = cornerView.bounds;
+    maskLayer1.path = maskPath1.CGPath;
+    
+    // 这些设置全没用了
+    cornerView.layer.shadowOffset = CGSizeMake(10, 10); // default (0, -3);
+    cornerView.layer.shadowColor = [UIColor blueColor].CGColor;
+    cornerView.layer.shadowOpacity = 1; // default 0，0 就看不到阴影
+    cornerView.layer.shadowRadius = 16; // 阴影的圆角
+    
+    
+    cornerView.layer.mask = maskLayer1;
 }
 
 
